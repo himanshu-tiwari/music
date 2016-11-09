8b59c7edf627fe35bd6d6c0ca7242b1687864257
himanshu tiwari <htiwarih@gmail.com>
2016-11-09 18:40:56 +0530

    Render templates using shortcuts

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Album
 
 # Create your views here.
@@ -8,11 +7,10 @@ from .models import Album
 
 def index(request):
 	all_albums = Album.objects.all()
-	template = loader.get_template('music/index.html')
 	context = {
 		'all_albums': all_albums,
 	}
-	return HttpResponse(template.render(context,request))
+	return render(request, 'music/index.html', context) 
 
 def detail(request, album_id):
 	return HttpResponse("<h2>Details for id " + str(album_id) + ":</h2>")
