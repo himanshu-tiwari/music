2b491da0812257bdc0d7833a2dbc0736ed07a3d2
himanshu tiwari <htiwarih@gmail.com>
2016-11-13 22:45:21 +0530

    Used context_object_name

--- music/views.py
+++ music/views.py
@@ -7,6 +7,7 @@ from .models import Album
 class IndexView(generic.ListView):
 	"""docstring for IndexView"""
 	template_name = "music/index.html"
+	context_object_name = "all_albums"
 
 	def get_queryset(self):
 		return Album.objects.all()
