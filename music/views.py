437a552cedeb1dd42af11522f70d90f19349a51f
himanshu tiwari <htiwarih@gmail.com>
2016-11-14 12:08:32 +0530

    Added UpdateView and DeleteView

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album
 
 # Create your views here.
@@ -23,3 +24,12 @@ class AlbumCreate(CreateView):
 	model = Album
 	fields = ['artist', 'album_title', 'genre', 'album_logo']
 		
+class AlbumUpdate(UpdateView):
+	"""docstring for AlbumUpdate"""
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
+		
+class AlbumDelete(DeleteView):
+	"""docstring for AlbumDelete"""
+	model = Album
+	success_url = reverse_lazy('music:index')		
